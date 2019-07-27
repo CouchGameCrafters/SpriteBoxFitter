d394b15b4f997676b3a016d653a572c9a9a64f46
CouchGameCrafters <53372039+CouchGameCrafters@users.noreply.github.com>
2019-07-27 14:53:24 +0200

    Add files via upload

---
+++ FitSpriteInBox.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(SpriteRenderer))]
+public class FitSpriteInBox : MonoBehaviour
+{
+    public Vector2 boxSize = Vector2.one;
+    private Vector2 lastSize = Vector2.zero;
+    private SpriteRenderer spriteRenderer;
+    public bool preserveAspect = true;
+
+
+    private void Start()
+    {
+        UpdateSpriteSize();        
+    }
+
+    private void Update()
+    {
+        UpdateSpriteSize();
+    }
+
+    private void UpdateSpriteSize ()
+    {
+        //if(lastSize == boxSize) return;
+        if(spriteRenderer == null) {
+            spriteRenderer = GetComponent<SpriteRenderer> ();
+            if(spriteRenderer == null) return;
+        }
+        if(spriteRenderer.sprite == null) return;
+
+        Vector2 size = spriteRenderer.sprite.rect.size / Mathf.Max(spriteRenderer.sprite.pixelsPerUnit, 0.001f);
+
+        if(preserveAspect) {
+            Vector2 difference = boxSize - size;
+            if(difference.x < difference.y) {
+                transform.localScale = Vector3.one * (boxSize.x / size.x);
+            } else {
+                transform.localScale = Vector3.one * (boxSize.y / size.y);
+            }
+        } else {
+            transform.localScale = new Vector3((boxSize.x / size.x), (boxSize.y / size.y), 1f);
+        }
+        
+
+        lastSize = boxSize;
+    }
+
+    #if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.gray;
+        Gizmos.DrawWireCube(transform.position, boxSize);
+    }
+    #endif
+}
